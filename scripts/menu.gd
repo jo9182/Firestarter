7f85236b41f44e91939a09555f79c3c3ebe4d949
jo9182 <73309930+jo9182@users.noreply.github.com>
2025-09-10 13:22:25 -0500

    Settings menu
    sliders and buttons don't work :(

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -7,7 +7,10 @@ func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 	
 
-
+func _on_settings_button_pressed() -> void:
+	var settings = load("res://scenes/settings.tscn").instantiate()
+	get_tree().current_scene.add_child(settings)
+	
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
