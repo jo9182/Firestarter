034395d9feeb9476e1c5f202dfb3e9868ee5dd5e
zHagerman <zander.hagerman@stu.daviess.kyschools.us>
2025-09-30 12:15:07 -0500

    Clear the commits to pull new project
    This should do nothing

--- scenes/levelTransitions/level.gd
+++ scenes/levelTransitions/level.gd
@@ -1,12 +1,13 @@
 class_name level extends Node2D
 
 func _ready() -> void:
+
 	self.y_sort_enabled = true
 	
 	PlayerManager.set_as_parent(self)
 	
 	global.level_load_started.connect( free_level )
-
+	
 	pass
 	
 func free_level() -> void:
