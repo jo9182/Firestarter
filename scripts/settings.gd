7b4612e569c0492f4c0407ad2bd90326375b8cc6
zHagerman <zander.hagerman@stu.daviess.kyschools.us>
2025-10-07 13:20:41 -0500

    implementation started
    connected the signs to poping up specific labels that connect to different games

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -7,14 +7,13 @@ func _ready():
 
 
 
-func _on_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0, value/5)
-
-
-
-func _on_mute_toggled(toggled_on: bool) -> void:
-	pass # Replace with function body.
-
-
 func _on_button_pressed() -> void:
 	queue_free()
+
+
+
+
+
+func _on_collect_pressed() -> void:
+	#Give wet rocks to inventory
+	pass
