b17b6d99c29f55d0a1144f888f3fd4b084313ea9
zHagerman <zander.hagerman@stu.daviess.kyschools.us>
2025-10-23 13:21:59 -0500

    Qte keys

---
+++ scripts/Key_Finder.gd
@@ -0,0 +1,22 @@
+class_name key_finder extends Node
+
+var dubbleyou : String = "W"
+var eh : String = "A"
+var es : String = "S"
+var dee : String = "D"
+var chosen1 : String
+
+
+func _ready() -> void:
+	#if Input.action_just_pressed("rand1"):
+		#if chosen1 = dubbleyou:
+			
+	pass
+	
+	
+func _process(delta: float) -> void:
+	
+	pass
+	
+func choose_wasd() -> void:
+	pass
