d19979d624a2bdf0ab832dd71889f00fe6389c23
zHagerman <zander.hagerman@stu.daviess.kyschools.us>
2025-10-23 11:57:15 -0500

    manual + sprites for resources
    95% finished manual

--- scripts/manual.gd
+++ scripts/manual.gd
@@ -21,26 +21,33 @@ func _on_right_pressed() -> void:
 		pos = 2
 		description.text = str("The logs go on last in a teepee form so that the fire can burn longer with the logs supporting it")
 		pg_number.text = str("page 2")
+		image.texture = load("res://scenes/popups/woods game/log.png")
 		left.visible = true
 		return
 	elif pos == 2:
 		pos = 3
 		description.text = str("The kindeling goes on third so that the fire has fuel to burn")
 		pg_number.text = str("page 3")
+		image.texture = load("res://sprites/objects/sticks-removebg-preview.png")
 	elif pos == 3:
 		pos = 4
 		description.text = str("The tinder will go on second after the dry rocks so that the fire can catch")
 		pg_number.text = str("page 4")
+		image.texture = load("res://sprites/objects/tinder-removebg-preview.png")
 		return
 	elif pos == 4:
 		pos = 5
 		description.text = str("Never build a fire with wet rocks as the water will expand in the rocks and they will explode")
 		pg_number.text = str("page 5")
+		image.texture = load("res://sprites/objects/Rocks-removebg-preview.png")
+		image.modulate = Color.DIM_GRAY
 		return
 	elif pos == 5:
 		pos = 6
-		description.text = str("There are lots of signs by the lake that can give you more helpful tips")
+		description.text = str("There are lots of signs by the lake that can give you more helpful tips you can interact with all signs by pressing 'Z' ")
 		pg_number.text = str("page 6")
+		image.texture = load("res://sprites/objects/sign-removebg-preview.png")
+		image.modulate = Color.WHITE
 		right.visible = false
 		return
 	pass # Replace with function body.
@@ -51,26 +58,33 @@ func _on_left_pressed() -> void:
 		pos = 1
 		description.text = str("Dry rocks are found to the north and are needed to be placed first in a circle to prevent fire from spreading uncontrollably")
 		pg_number.text = str("page 1")
+		image.texture = load("res://sprites/objects/Rocks-removebg-preview.png")
 		left.visible = false
 	elif pos == 3:
 		pos = 2
 		description.text = str("The logs go on last in a teepee form so that the fire can burn longer with the logs supporting it")
 		pg_number.text = str("page 2")
+		image.texture = load("res://scenes/popups/woods game/log.png")
 		return
 	elif pos == 4:
 		pos = 3
 		description.text = str("The kindeling goes on third so that the fire has fuel to burn")
 		pg_number.text = str("page 3")
+		image.texture = load("res://sprites/objects/sticks-removebg-preview.png")
 		return
 	elif pos == 5:
 		pos = 4
 		description.text = str("The tinder will go on second after the dry rocks so that the fire can catch")
 		pg_number.text = str("page 4")
+		image.texture = load("res://sprites/objects/tinder-removebg-preview.png")
+		image.modulate = Color.WHITE
 		return
 	elif pos == 6:
 		pos = 5
 		description.text = str("Never build a fire with wet rocks as the water will expand in the rocks and they will explode")
 		pg_number.text = str("page 5")
+		image.texture = load("res://sprites/objects/Rocks-removebg-preview.png")
+		image.modulate = Color.DIM_GRAY
 		right.visible = true
 		return
 	pass # Replace with function body.
